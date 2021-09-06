172ed845641d15061e023cd5e9fa4e1541566ed3
shahnk19 <shahnk19@gmail.com>
2021-09-06 09:44:21 +0800

    Add user following

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_23_020401) do
+ActiveRecord::Schema.define(version: 2021_08_29_233801) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2021_08_23_020401) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
